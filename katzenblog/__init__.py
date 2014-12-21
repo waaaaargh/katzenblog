ddcb4371b8c0662181d8fbd760ed90d410c4c96f
Johannes Fürmann <johannes@weltraumpflege.org>
2014-12-21 03:26:32 +0100

    add config file

--- katzenblog/__init__.py
+++ katzenblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_pyfile('../katzenblog.cfg')
 db = SQLAlchemy(app)
 
 from katzenblog.model import *
