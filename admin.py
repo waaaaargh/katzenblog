eebe567c55b0ee0d033b0bc46997e0e504113f31
Johannes Fürmann <johannes@weltraumpflege.org>
2014-12-21 03:26:05 +0100

    remove old unittest command, run nosetests.

--- admin.py
+++ admin.py
@@ -6,11 +6,5 @@ from katzenblog import app
 
 manager = Manager(app)
 
-@manager.command
-def run_tests():
-    from tests import *
-    import unittest
-    unittest.main()
-
 if __name__ == '__main__':
     manager.run()
