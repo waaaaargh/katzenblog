4099dada9c032b5fe74b65e340833962b3705e8e
Johannes Fürmann <johannes@weltraumpflege.org>
2014-12-21 03:31:34 +0100

    add create_db command to admin.py

--- admin.py
+++ admin.py
@@ -2,9 +2,13 @@
 
 from flask.ext.script import Manager
 
-from katzenblog import app
+from katzenblog import app, db
 
 manager = Manager(app)
+
+@manager.command
+def create_db():
+    db.create_all()
 
 if __name__ == '__main__':
     manager.run()
